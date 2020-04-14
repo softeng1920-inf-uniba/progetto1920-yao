ef7bb4b15e25ac6e38c9fab8ce2132dbfd5345dd
Andre Alvizuri <andredavid.adam@gmail.com>
2020-04-14 19:23:05 +0200

    aggiunta della classe Pedone

---
+++ src/main/java/scacchi/Pedone.java
@@ -0,0 +1,16 @@
+public class Pedone extends Pezzo{
+   Pedone(int colore ,  Posizione pos){
+       super(2 , colore , "p" , pos );
+       super.setEmpoissonTrue();
+       if (colore == 0) {
+           super.setSimbolo('\u265F');
+       }
+       else{
+           super.setSimbolo('\u2659');
+       }
+   }
+}
+
+
+
+
