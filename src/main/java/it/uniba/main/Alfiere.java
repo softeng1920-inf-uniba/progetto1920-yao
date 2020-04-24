62a0c586d820ed2bb537943ba8ce306461eb8f55
Federico Chiaradia <chiaradiafederico0@gmail.com>
2020-04-24 20:14:53 +0200

    Aggiunta classe Alfiere(Senza controlli movimento)

---
+++ src/main/java/it/uniba/main/Alfiere.java
@@ -0,0 +1,13 @@
+package it.uniba.main;
+
+public class Alfiere extends Pezzo{
+	    Alfiere(int colore ,  Posizione pos){
+	        super(8 , colore , 'B' , pos );
+	        if (colore == 0) {
+	            super.setSimbolo('\u2657');
+	        }
+	        else{
+	            super.setSimbolo('\u265D');
+	        }
+	    }
+	}
