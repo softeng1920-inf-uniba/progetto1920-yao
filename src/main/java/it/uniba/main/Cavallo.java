b360016b0cc30fd2a0c0896b0c8ca0522a7070bc
pete <pietro.conca.pc@gmail.com>
2020-05-05 17:01:50 +0200

    aggiunta la classe cavallo

---
+++ src/main/java/it/uniba/main/Cavallo.java
@@ -0,0 +1,14 @@
+  
+package it.uniba.main;
+
+public class Cavallo extends Pezzo{
+    Cavallo(int colore , Posizione pos){
+        super(2 , colore , 'N' , pos );
+        if (colore == 0) {
+            super.setSimbolo('\u265E');
+        }
+        else{
+            super.setSimbolo('\u2658');
+        }
+    }
+}
