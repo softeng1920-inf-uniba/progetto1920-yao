2351545167731e8d3c6c0d9d9cdfd909c632ef07
peteknk <61543521+peteknk@users.noreply.github.com>
2020-05-23 18:17:46 +0200

    aggiunta check style classe pedone

--- src/main/java/it/uniba/main/Pedone.java
+++ src/main/java/it/uniba/main/Pedone.java
@@ -4,21 +4,20 @@ package it.uniba.main;
  * responsabilita: rappresenta il pezzo di tipo pedone
  */
 
-public class Pedone extends Pezzo{
-    Pedone(int colore ,  Posizione pos){
-        super(2 , colore , 'p' , pos );
-        super.setEmpoissonTrue();
-        if (colore == 0) {
-            super.setSimbolo('\u265F');
-        }
-        else{
-            super.setSimbolo('\u2659');
-        }
+public class Pedone extends Pezzo {
+  Pedone(int colore, Posizione pos) {
+    super(2, colore, 'p', pos);
+    super.setEmpoissonTrue();
+    if (colore == 0) {
+      super.setSimbolo('\u265F'); //pedone nero
+    } else {
+      super.setSimbolo('\u2659'); //pedone bianco
     }
+  }
 
-    @Override
-    public void giaMosso() {
-        super.giaMosso();
-        setGittata(1);
-    }
+  @Override
+  public void giaMosso() {
+    super.giaMosso();
+    setGittata(1);
+  }
 }
